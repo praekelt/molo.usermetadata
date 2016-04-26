be576b63a52b09d828526eae6d0395745e016310
codiebeulaine <codiebeulaine@gmail.com>
2016-04-26 19:25:45 +0200

    updating urls and views

--- molo/usermetadata/urls.py
+++ molo/usermetadata/urls.py
@@ -6,4 +6,10 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     '',
     url(r'^persona/$', views.PersonaView.as_view(), name='persona'),
+    url(
+        r'^persona/(?P<persona_id>\d+)$',
+        views.SetPersonaView, name='set_persona'),
+    url(
+        r'^persona/skip$',
+        views.SkipPersonaView, name='skip_persona')
 )
--- molo/usermetadata/views.py
+++ molo/usermetadata/views.py
@@ -1,14 +1,29 @@
 from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
 
-from molo.usermetadata.models import PersonaPage, PersonaIndexPage
+from molo.usermetadata.models import PersonaPage
 
 
 class PersonaView(TemplateView):
     template_name = 'persona.html'
 
     def get_context_data(self, *args, **kwargs):
-        index_page = PersonaIndexPage.objects.live().first()
-        persona_pages = PersonaPage.objects.live().child_of(index_page)
+        persona_pages = PersonaPage.objects.live().all()
         context = super(PersonaView, self).get_context_data(*args, **kwargs)
-        context.update({'persona_pages': persona_pages, 'index_page': index_page})
+        context.update({
+            'persona_pages': persona_pages,
+            'next': self.request.GET.get('next')})
         return context
+
+
+def SetPersonaView(request, persona_id):
+    persona = get_object_or_404(PersonaPage, pk=persona_id)
+    if not ('MOLO_PERSONA_SELECTION') in request.session:
+        request.session['MOLO_PERSONA_SELECTION'] = persona.slug
+        return HttpResponseRedirect(request.GET.get('next', '/'))
+
+
+def SkipPersonaView(request):
+    request.session['MOLO_PERSONA_SELECTION'] = ''
+    return HttpResponseRedirect(request.GET.get('next', '/'))
