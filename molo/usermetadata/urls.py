b127c420e65e7d84ab5c9c62514787a85175202c
Saeed Marzban <Saeed.mb8@gmail.com>
2016-04-28 18:02:41 +0200

    small fix

--- molo/usermetadata/urls.py
+++ molo/usermetadata/urls.py
@@ -7,9 +7,9 @@ urlpatterns = patterns(
     '',
     url(r'^persona/$', views.PersonaView.as_view(), name='persona'),
     url(
-        r'^persona/(?P<persona_id>\d+)/$',
-        views.set_persona, name='set_persona'),
-    url(
         r'^persona/skip/$',
         views.skip_persona, name='skip_persona'),
+    url(
+        r'^persona/(?P<persona_id>\d+)/$',
+        views.set_persona, name='set_persona'),
 )
