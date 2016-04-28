4d2ffb94a066b06c6e62601a4e11e7aebed046a8
Saeed Marzban <Saeed.mb8@gmail.com>
2016-04-28 22:14:12 +0200

    added persona settings

--- molo/usermetadata/middleware.py
+++ molo/usermetadata/middleware.py
@@ -1,31 +1,38 @@
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 from django.conf import settings
+from wagtail.wagtailcore.models import Site
+from wagtail.contrib.settings.context_processors import SettingsProxy
 
 
 class PersonaMiddleware(object):
 
     def process_request(self, request):
-        exclude = [
-            settings.MEDIA_URL,
-            settings.STATIC_URL,
-            reverse('molo.usermetadata:persona'),
-            reverse('health'),
-            reverse('versions'),
-            '/admin/',
-            'django-admin/',
-            '/import/',
-            '/import/',
-            '/locale/'
-        ]
 
-        if hasattr(settings, 'PERSONA_IGNORE_PATH'):
-            exclude += settings.PERSONA_IGNORE_PATH
+        site = Site.objects.get(is_default_site=True)
+        setting = SettingsProxy(site)
+        persona_settings = setting['usermetadata']['PersonaeSettings']
+        if persona_settings.persona_required:
+            exclude = [
+                settings.MEDIA_URL,
+                settings.STATIC_URL,
+                reverse('molo.usermetadata:persona'),
+                reverse('health'),
+                reverse('versions'),
+                '/admin/',
+                'django-admin/',
+                '/import/',
+                '/import/',
+                '/locale/'
+            ]
 
-        if any([path for path in exclude if request.path.startswith(path)]):
-            return None
+            if hasattr(settings, 'PERSONA_IGNORE_PATH'):
+                exclude += settings.PERSONA_IGNORE_PATH
 
-        if 'MOLO_PERSONA_SELECTION' not in request.session:
-            url = '%s?next=%s' % (reverse('molo.usermetadata:persona'),
-                                  request.path)
-            return redirect(url)
+            if any([p for p in exclude if request.path.startswith(p)]):
+                return None
+
+            if 'MOLO_PERSONA_SELECTION' not in request.session:
+                url = '%s?next=%s' % (reverse('molo.usermetadata:persona'),
+                                      request.path)
+                return redirect(url)
