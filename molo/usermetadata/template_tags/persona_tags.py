423fb26b56708e1e0192a84bfe44b8a71593563d
Codie Roelf <codie@praekelt.com>
2016-06-29 11:20:31 +0200

    correct the persona tag

--- molo/usermetadata/template_tags/persona_tags.py
+++ molo/usermetadata/template_tags/persona_tags.py
@@ -6,8 +6,8 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def persona_selected(context):
     request = context['request']
-    if 'MOLO_PERSONA_SELECTED':
-        selected_persona = request.session['MOLO_PERSONA_SELECTED']
+    if 'MOLO_PERSONA_SELECTED' in request.session:
+        selected_persona = request.session['MOLO_PERSONA_SELECTION']
         del request.session['MOLO_PERSONA_SELECTED']
         return selected_persona
     return None
